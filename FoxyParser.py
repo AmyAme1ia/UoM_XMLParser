f3c22609c5f71b3b623dc1899ca501ecd69706d9
matt <mattwherlock@gmail.com>
2016-11-10 10:41:03 +0000

    beginnings

--- FoxyParser.py
+++ FoxyParser.py
@@ -1,25 +1,14 @@
+# Python 3.5
 import xml.etree.ElementTree as ET
 
-filename = input('Please enter filename: ')
-# filename = 'LRG_62.xml'
-print(filename)
 
-tree = ET.parse(filename)
-root = tree.getroot()
+def check_file(file_name):
+    ''' check the input file is an LRG '''
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    
+    assert = root.tag == 'LRG', 'Input file must be an LRG - do it again doofus'
+    
+    return root
 
-# for child in root:
-#     print(child.tag, child.attrib)
-#
-# print('In fixed')
-# for child in root[0]:
-#     print(child.tag, child.attrib, child.text)
-#
-# print('In updateable')
-# for child in root[1]:
-#     print(child.tag, child.attrib)
-
-meep = root.findall("./fixed_annotation/source/contact")[0].tag
-moop = root.findall("./fixed_annotation/source/contact/email")[0].text
-print(meep)
-print(moop)
-
+check_file('LRG_62.xml')
