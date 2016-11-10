4d110f5fbf9bb3ef9f0ab614e7879316c4574915
matt <mattwherlock@gmail.com>
2016-11-10 11:23:50 +0000

    first assertions to check file type

--- FoxyParser.py
+++ FoxyParser.py
@@ -1,14 +1,17 @@
 # Python 3.5
 import xml.etree.ElementTree as ET
 
-
+def xml_checker(file_name):
+    '''Runs a check to confirm a .xml file has been entered'''
+    assert file_name.endswith('.xml'), ' wrong file type entered'
+    return(check_file(file_name))
+    
 def check_file(file_name):
-    ''' check the input file is an LRG '''
+    ''' check the input file is an LRG and return root '''    
     tree = ET.parse(file_name)
     root = tree.getroot()
-    
-    assert = root.tag == 'LRG', 'Input file must be an LRG - do it again doofus'
-    
-    return root
+    assert root.tag == 'lrg', 'Input file must be an LRG'
+    return(root)
 
-check_file('LRG_62.xml')
+print(xml_checker('LRG_62_dud.xml'))
+#print(xml_checker('wrong.txt'))
